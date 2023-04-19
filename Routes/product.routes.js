3f5a887b908926d248b30cbea0166331fae03fd5
Jagroshan <jagroshansingh615@gmail.com>
2023-04-19 19:06:12 +0530

    changed json to express

---
+++ Routes/product.routes.js
@@ -0,0 +1,23 @@
+const express=require('express')
+const { productModel } = require('../model/productModel')
+const productrouter=express.Router()
+
+productrouter.get('/',async(req,res)=>{
+    try {
+        let see=await productModel.find({'location':req.query.location})
+        res.send(see)
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+productrouter.get('/single',async(req,res)=>{
+    try {
+        let see=await productModel.find({'id':req.query.id})
+        res.send(see)
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+module.exports={productrouter}
